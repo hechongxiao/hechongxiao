0f3d73ead8b30651d5d356fac2168b76bd2195f8
hechongxiao <hechongxiao2015@u.northwestern.edu>
2016-04-25 20:33:23 -0500

    hw4
    hw3 is actually hw4

---
+++ 433/hw3/HW3HECHONGXIAO.X/build/default/production/333.o.d
@@ -0,0 +1,3 @@
+build/default/production/333.o:  \
+333.c  \
+i2c_master_noint.h 
---
+++ 433/hw3/HW3HECHONGXIAO.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=HW3HECHONGXIAO.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/HW3HECHONGXIAO.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=hw3hechongxiao.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/hw3hechongxiao.x.tar
